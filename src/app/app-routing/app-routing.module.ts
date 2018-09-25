e70f6c4313edccc2af7c8b4b1ad4f658325ae5e3
Mat Harrington <mat_harrington@dialog.com.au>
2018-09-25 11:20:53 +1000

    Removed unused routes

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StoryFeedComponent } from '../components/story-feed/story-feed.component';
-import { StoryFeedItemDetailComponent } from '../components/story-feed-item-detail/story-feed-item-detail.component';
 
-// Unused now in minimal state. Would enable /[subdomain] style navigation to each of the below endpoints.
+// In minimal state. Would enable /[subdomain] style navigation to each of the below endpoints.
 const routes: Routes = [
   { path: 'app-story-feed', data: {  }, component: StoryFeedComponent },
-  { path: 'topstories', data: { feed: 'topstories' }, component: StoryFeedComponent },
-  { path: '', redirectTo: 'topstories', pathMatch: 'full' },
-  { path: 'newstories', data: { feed: 'newstories' }, component: StoryFeedComponent },
-  { path: 'beststories', data: { feed: 'beststories' }, component: StoryFeedComponent },
-  { path: 'askstories', data: { feed: 'askstories' }, component: StoryFeedComponent },
-  { path: 'showstories', data: { feed: 'showstories' }, component: StoryFeedComponent },
-  { path: 'jobstories', data: { feed: 'jobstories' }, component: StoryFeedComponent },
-  { path: 'item/:itemId', component: StoryFeedItemDetailComponent }
+  { path: '', redirectTo: 'app-story-feed', pathMatch: 'full' },
 ];
 
 @NgModule({
